99f4ec7e2a7bfe681787efe9cd2939191ba959f6
César <cesar_delse@hotmail.com>
2022-04-27 03:02:25 -0300

    Guardamos Trabajo parcial de interfaz de usuario

---
+++ CAD.Base.Web2/wwwroot/js/custom.js
@@ -0,0 +1,9 @@
+﻿window.MyInterop = (function () {
+    const _changeTheme = function (classToAdd) {
+        document.body.classList.add(classToAdd);
+    };
+
+    return {
+        ChangeTheme: _changeTheme
+    };
+})();
